ab09f7ce1bda331c26654775bcf0f9ba1ace5320
Akansha Kamble <akanshakamble10@gmail.com>
2026-04-12 20:51:16 +0530

    javascript code

---
+++ program.js
@@ -0,0 +1,20 @@
+// Basic JavaScript program
+
+console.log("Hello, GitHub!");
+console.log("This is my first JavaScript file 🚀");
+
+// Simple function
+function greet(name) {
+    return `Hello, ${name}! Welcome to JavaScript.`;
+}
+
+// Calling the function
+let message = greet("Akansha");
+console.log(message);
+
+// Simple calculation
+let a = 10;
+let b = 5;
+
+console.log("Sum =", a + b);
+console.log("Difference =", a - b);
