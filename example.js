0819b646b3bd509ea8beed28811caa16a9b265e7
Akansha Kamble <akansha.kamble@infoteksoftware.com>
2026-04-12 16:52:17 +0530

    first commit

---
+++ example.js
@@ -0,0 +1,33 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  // launch browser
+  const browser = await chromium.launch({ headless: false });
+
+  // create new context
+  const context = await browser.newContext();
+
+  // open new page
+  const page = await context.newPage();
+
+  // go to Playwright website
+  await page.goto('https://playwright.dev');
+
+  // click "Get started" and wait for navigation
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('text=Get started')
+  ]);
+
+  // wait for a visible element on next page
+  await page.waitForSelector('h1');
+
+  // take screenshot
+  await page.screenshot({ path: 'playwright.png' });
+
+  // wait so you can see result
+  await page.waitForTimeout(3000);
+
+  // close browser
+  await browser.close();
+})();
